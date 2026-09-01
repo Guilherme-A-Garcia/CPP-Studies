b9b3aa036fe89e494095c9f95befe99cf4a48956
Guilherme G. <guilherme.garcia.dev.br@gmail.com>
2026-08-31 22:41:30 -0300

    feat: finish the first C++ class

---
+++ C++ Class 1/class1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+// "#include" acts basically import in Java or Python, in a superficial level.
+// "iostream" is a headerfile that contains functions for basic i/o operations.
+
+// v main function v
+int main(){
+    std::cout << "Hello world!\n"; // std = standard, cout = char output.
+    // "<<" = outputs
+    
+    std::cout << "This is the second str output line." << std::endl; 
+    // std::endl is the same as \n, but it also flushes the output buffer.
+    
+    // \n is better performance-wise, but it doesn't flush the buffer.
+
+    /*
+    Multi-line comments
+    also work in C++!
+    */
+
+    return 0;  // 0 = "works fine", 1 = "something went wrong"
+}
